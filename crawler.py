2be803f7a9ead3d42db3ba9e9ae1c479e50f50c6
Igor Augusto Ferreira Carlos <igor.augusto@ibm.com>
2022-10-24 18:14:29 -0300

    First commit fo web crawler in python

---
+++ crawler.py
@@ -0,0 +1,16 @@
+import requests
+import re
+
+url = "http://www.revistabula.com/7073-a-lista-definitiva-dos-100-melhores-filmes-de-todos-os-tempos/"
+url2 = "https://www.nba.com/stats/players/traditional"
+check = []
+
+r = requests.get(url2)
+html = r.text
+search = re.findall(r'<a href=[\'"?](https[://\w\-._]+)', html)
+
+for link in search:
+    if link not in check:
+        check.append(link)
+        with open("link2.txt", "a") as file:
+            file.write(f'{link}\n')
